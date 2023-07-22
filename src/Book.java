ada45aba9b2a22b4c2cea5fbe8db2a3822453b24
jawuthamy <thirujawuthamy007@gmail.com>
2023-07-22 11:42:48 +0530

    book class

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,47 @@
 public class Book {
-    private String isbn;
+    private long   isbn;
+    private String title;
+    private String category;
+    private String author;
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(long isbn, String title, String category, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.category = category;
+        this.author = author;
+    }
+
+
 }
